79f9b7c2a4e407c30cfeb0d2fdddb299c7e49c17
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-01-29 14:05:28 +0530

    Add files via upload

---
+++ IOT/Temperature.py
@@ -0,0 +1,10 @@
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+
+GPIO.setmode (GPIO.BCM)
+#Using BCM means use GPIO3 Pin to cennect to sensor
+
+while(True):
+    humidity,temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,3)
+    print(humidity,temperature)
+GPIO.cleanup()   
